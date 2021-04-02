6417a185da31161478e9186f950c1131081464f5
rdogm13 <drg6525@yahoo.com>
2021-04-02 15:23:08 -0700

    modified email and price field types in customer and product models

--- store/models.py
+++ store/models.py
@@ -6,14 +6,14 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
     digital = models.BooleanField(default=False,null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
