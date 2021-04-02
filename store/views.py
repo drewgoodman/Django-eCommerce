448381afa3b14bff54c5e4462243a01d3ad181ea
rdogm13 <drg6525@yahoo.com>
2021-04-01 18:19:31 -0700

    cart add button creates order and modifies order item count

--- store/views.py
+++ store/views.py
@@ -45,4 +45,19 @@ def updateItem(request):
     action = data['action']
     print('Action', action)
     print('Product', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity += 1
+    elif action == 'remove':
+        orderItem.quantity -= 1
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
     return JsonResponse('Item was added', safe=False)
